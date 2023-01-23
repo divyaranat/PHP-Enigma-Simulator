2174c9eefac9402de7030c0796a9613636bd812b
Divya Ranat <38928132+divyaranat@users.noreply.github.com>
2023-01-23 13:57:49 -0800

    Add getTurnoverPoint functions.

--- Rotors.php
+++ Rotors.php
@@ -1,25 +1,40 @@
 <?php
+
     Class Rotors {
         private $rotorOneObject;
         private $rotorTwoObject;
         private $rotorThreeObject;
+        private $reflector;
 
-        function __construct($rotorOneObject, $rotorTwoObject, $rotorThreeObject) {
+        function __construct($rotorOneObject, $rotorTwoObject, $rotorThreeObject, $reflector) {
             $this->rotorOneObject = $rotorOneObject;
             $this->rotorTwoObject = $rotorTwoObject;
             $this->rotorThreeObject = $rotorThreeObject;
+            $this->reflector = $reflector;
         }
 
         function getRotorOne() {
-            return $this->rotorOneObject;
+            return $this->rotorOneObject->getRotorArray();
+        }
+
+        function getRotorOneTurnoverPoint() {
+            return $this->rotorOneObject->getTurnoverPoint();
         }
 
         function getRotorTwo() {
-            return $this->rotorTwoObject;
+            return $this->rotorTwoObject->getRotorArray();
+        }
+
+        function getRotorTwoTurnoverPoint() {
+            return $this->rotorTwoObject->getTurnoverPoint();
         }
         
         function getRotorThree() {
-            return $this->rotorThreeObject;
+            return $this->rotorThreeObject->getRotorArray();
+        }
+
+        function getReflector() {
+            return $this->reflector;
         }
     }
 ?>
