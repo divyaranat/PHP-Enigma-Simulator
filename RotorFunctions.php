1a4f03ed528b895bf802d89cb7352184aaf8a012
Divya Ranat <38928132+divyaranat@users.noreply.github.com>
2023-01-23 13:56:57 -0800

    Start traverseRotors function.

--- RotorFunctions.php
+++ RotorFunctions.php
@@ -1,4 +1,7 @@
 <?php
+    require "Rotor.php";
+    require "Rotors.php";
+
     function rotateRotor($rotorArray) {
         $tempIndexHolderZero = $rotorArray[0][0];
         $tempIndexHolderOne = $rotorArray[0][1];
@@ -16,6 +19,28 @@
         for ($i = 0; $i <= 25; $i++) {
             if ($rotorArray[$arraySelect][$i] == $result) {
                 return $i;
+            }
+        }
+    }
+
+    function traverseRotors($letterMessage, $rotorsObject) {
+        $rotorOneArray = $rotorsObject->getRotorOne();
+        $rotorTwoArray = $rotorsObject->getRotortwo();
+        $rotorThreeArray = $rotorsObject->getRotorThree();
+        $reflectorArray = $rotorsObject->getReflector();
+
+        $rotorOneTurnoverPoint = $rotorsObject->getRotorOneTurnoverPoint();
+        $rotorTwoTurnoverPoint = $rotorsObject->getRotorTwoTurnoverPoint();
+
+        rotateRotor($rotorOneArray);
+        $rotorOneCurrentTurnoverPoint = $rotorOneArray[0][25];
+
+        if ($rotorOneTurnoverPoint == $rotorOneCurrentTurnoverPoint) {
+            rotateRotor($rotorTwoArray);
+            $rotorTwoCurrentTurnoverPoint = $rotorTwoArray[0][25];
+
+            if ($rotorTwoTurnoverPoint == $rotorTwoCurrentTurnoverPoint) {
+                rotateRotor($rotorTwoArray);
             }
         }
     }
